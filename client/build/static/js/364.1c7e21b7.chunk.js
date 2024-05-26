8b90510d852c40c22bbad28c42571b7749fdaa9b
Siddharth Nair <nairsid2@gmail.com>
2024-05-26 19:09:02 -0400

    removed build folder

--- client/build/static/js/364.1c7e21b7.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkclient=self.webpackChunkclient||[]).push([[364],{9364:(e,n,s)=>{s.r(n),s.d(n,{default:()=>d});var c=s(5468),t=s(2791),r=s(5139),l=s(184);const a=()=>(0,l.jsxs)(c.YI,{children:[(0,l.jsx)(c.WD,{name:"App",children:(0,l.jsx)(c.wA,{name:"Loader",children:(0,l.jsx)(c.$e,{children:(0,l.jsx)(i,{})})})}),(0,l.jsx)(r.Z,{})]});function i(){return(0,l.jsx)(t.Fragment,{children:"Loading..."})}const d=()=>(0,l.jsx)(c.Vt,{palette:(0,l.jsx)(a,{})})}}]);
-//# sourceMappingURL=364.1c7e21b7.chunk.js.map
